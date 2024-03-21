f17d7cb9cd281adf585af12f6525eabc3edbae67
dmitryulkin <dmulkin@yandex.ru>
2024-03-21 23:07:23 +0000

    feat: add postCreate Command for container to do anything with mounted volume

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
+# /workspase is inaccessible on image creation it is mounted after that
+# here are modification on mounted volume with project
+
+poetry check
+poetry install --no-interaction --no-cache --no-root
 
 sudo chown -R nonroot /workspace
