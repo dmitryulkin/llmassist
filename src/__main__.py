554e28efb2d7ac3f33c85517f96fd09f4532784b
dmitryulkin <dmulkin@yandex.ru>
2024-03-25 19:54:45 +0000

    feat: test gpt4free as LLM sourse

--- src/__main__.py
+++ src/__main__.py
@@ -1,1 +1,27 @@
-print('Hello world')
+import asyncio
+import logging
+
+from g4f import Provider
+from g4f.client import Client
+
+
+def start_llmassist():
+    client = Client(
+        provider=Provider.RetryProvider(
+            [Provider.Liaobots],
+            shuffle=False
+        )
+    )
+    response = client.chat.completions.create(
+        model="",
+        messages=[{"role": "user", "content": "Hello"}],
+    )
+    print(response.choices[0].message.content)
+
+
+async def main():
+    await asyncio.to_thread(start_llmassist)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
