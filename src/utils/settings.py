efcea351df841cb128e94f6f42bdec66cd9a44d4
dmitryulkin <dmulkin@yandex.ru>
2024-04-26 20:54:17 +0000

    feat: change settings for tor and log

---
+++ src/utils/settings.py
@@ -0,0 +1,71 @@
+import logging
+from typing import Tuple, Type
+
+from pydantic import PositiveInt, ValidationInfo, field_validator
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
+
+
+class Settings(BaseSettings):
+    # secrets
+
+    # .env
+    # general settings
+    LOG_LEVEL: int = logging.DEBUG
+    LOG_TO_FILE: bool = False
+
+    # proxy settings
+    # tor
+    USE_TOR: bool = False
+    TOR_SOCKS5_PORT: PositiveInt | None = None
+
+    @field_validator("LOG_LEVEL", mode="before")
+    @classmethod
+    def transform_log_level_str_to_int(cls, value: int | str) -> int:
+        value = value if type(value) is int else value.casefold()
+        match value:
+            case "debug" | logging.DEBUG:
+                return logging.DEBUG
+            case "info":
+                return logging.INFO
+            case "warn" | "warning":
+                return logging.WARNING
+            case "error":
+                return logging.ERROR
+            case _:
+                raise ValueError(".env LOG_LEVEL is incorrect")
+
+    @field_validator("TOR_SOCKS5_PORT", mode="after")
+    @classmethod
+    def check_tor_port(
+        cls, value: PositiveInt | None, info: ValidationInfo
+    ) -> PositiveInt | None:
+        if info.data["USE_TOR"]:
+            assert value is not None, "USE_TOR=1. TOR_SOCKS5_PORT is required."
+        return value
+
+    # read only .env and secrets
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        # here we choose to ignore arguments from init_settings
+        return dotenv_settings, file_secret_settings
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+        # secrets_dir='keys',
+    )
+
+
+settings = Settings()
